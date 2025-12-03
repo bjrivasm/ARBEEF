fd09e20f5b186badedfd9775f75c4c38652d5991
javihergomm <153815458+javihergomm@users.noreply.github.com>
2025-12-03 19:06:39 +0100

    Simon dice
    pocho

---
+++ Assets/Scripts/SimonSays.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SimonSays : MonoBehaviour
+{
+    int[] secuence = new int[4];
+    int playerIndex = 0;
+    int lvl = 4;
+    void Start()
+    {
+    
+        StartCoroutine(PlaySimonSays());
+        
+    }
+
+    IEnumerator PlaySimonSays()
+    {
+        for (int i = 0; i < secuence.Length; i++)
+        {
+            secuence[i] = Random.Range(1 , 4 + 1);
+        }
+
+        for (int round = 1; round <= lvl; round++)
+        {
+
+
+            for (int i = 0; i < round; i++)
+            {
+                Debug.Log(secuence[i]);
+                yield return new WaitForSeconds(2f);
+            }
+
+            playerIndex = 0;
+
+            while (playerIndex < round)
+            {
+                bool recieved = false;
+                float time = 0f;
+
+                while (!recieved && time < 5f)
+                {
+                    int direction = ReadKey();
+                    if (direction != 0)
+                    {
+                        recieved = true;
+                        if (direction == secuence[playerIndex])
+                        {
+                            Debug.Log("BIEN!");
+                            playerIndex++;
+                        }
+                        else
+                        {
+                            Debug.Log("Tonto, asi no era, que eres tonto");
+                            yield break;
+                        }
+
+
+
+                    }
+                    time += Time.deltaTime;
+                    yield return null;
+
+
+                }
+                if (!recieved)
+                {
+                    Debug.Log("Broski hay que ser mas rapido eh!");
+                    yield break;
+                }
+
+            }
+            Debug.Log("Pos mu bien");
+        }
+        Debug.Log("Joder eres weno e");
+    }
+
+    int ReadKey()
+    {
+        if(Keyboard.current.upArrowKey.wasPressedThisFrame) return 1;
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame) return 2;
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame) return 3;
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame) return 4;
+        return 0;
+    }
+}
