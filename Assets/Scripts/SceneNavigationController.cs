ef089216db1e151a4302c3c7cce8f529d4bd86a4
bjrivasm <bryan.rivas@educa.madrid.org>
2025-12-07 14:13:12 +0100

    Fixed SceneNavigationController and deleted animations

--- Assets/Scripts/SceneNavigationController.cs
+++ Assets/Scripts/SceneNavigationController.cs
@@ -1,58 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using System.Collections;
 
 public class SceneNavigationController : MonoBehaviour
 {
-    public float duration = 0.2f;
-    private bool pressed = false;
-
-    public void OnClickPlay(Button button)
+    public void OnClickPlay()
     {
-        if (!pressed) StartCoroutine(ScaleButton(button.transform, () =>
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }));
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+        AdManager.instance.ShowAd();
     }
 
-    public void OnClickExit(Button button)
+    public void OnClickExit()
     {
-        if (!pressed) StartCoroutine(ScaleButton(button.transform, () =>
-        {
-            Application.Quit();
-        }));
+        Application.Quit();
+        Debug.Log("Cerrando la app...");
     }
 
-    public void PreviousScene() { 
-        int escenaActual = SceneManager.GetActiveScene().buildIndex; 
-        SceneManager.LoadScene(escenaActual - 1);
-    }
-
-    IEnumerator ScaleButton(Transform button, System.Action callback)
+    public void PreviousScene()
     {
-        pressed = true;
-        Vector3 original = button.localScale;
-        Vector3 target = original * 0.85f;
+        int index = SceneManager.GetActiveScene().buildIndex;
 
-        float t = 0;
-        while (t < 1)
-        {
-            t += Time.deltaTime / duration;
-            float scale = Mathf.SmoothStep(1f, 0.85f, t);
-            button.localScale = original * scale;
-            yield return null;
-        }
-
-        t = 0;
-        while (t < 1)
-        {
-            t += Time.deltaTime / duration;
-            float scale = Mathf.SmoothStep(0.85f, 1f, t);
-            button.localScale = original * scale;
-            yield return null;
-        }
-
-        callback?.Invoke();
+        if (index > 0)
+            SceneManager.LoadScene(index - 1);
     }
 }
